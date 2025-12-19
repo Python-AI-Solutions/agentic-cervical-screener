8fb2c9c8078876f64a4bc76530c3bcbdaa5c3815
leej3 <johnleenimh@gmail.com>
2025-12-19 15:16:18 +0000

    remove superfluous files

--- frontend/src/components/ui/index.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Component exports - Central export point for all UI components
- */
-
-export { Button, type ButtonProps } from './Button';
-export { Modal, ModalTrigger, type ModalProps, type ModalTriggerProps } from './Modal';
-export { Toggle, type ToggleProps } from './Toggle';
-export { Badge, type BadgeProps } from './Badge';
-
